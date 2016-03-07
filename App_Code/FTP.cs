c1ffbd8ba081ca9b5e06e99630f996c2f044e324
Alex Zhang <zyf1014@hotmail.com>
2016-03-06 19:16:15 -0800

    add FTP and FTPHelper class

---
+++ App_Code/FTP.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// Summary description for FTP
+/// </summary>
+public class FTP
+{
+
+    public FtpWebResponse Upload(string filePath, string hostName, NetworkCredential credentials, bool isPassiveMode)
+    {
+        FtpWebResponse response = null;
+        try
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(hostName + Path.GetFileName(filePath));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.UsePassive = isPassiveMode;
+            request.Credentials = credentials;
+            request.KeepAlive = true; // false;
+            request.Timeout = -1;
+
+            // Copy the contents of the file to the request stream.
+            StreamReader sourceStream = new StreamReader(filePath);
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(fileContents, 0, fileContents.Length);
+            }
+
+            response = (FtpWebResponse)request.GetResponse();
+            //Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+            response.Close();
+        }
+        catch (Exception ex)
+        {
+            string test = ex.Message;
+            //test = response.StatusDescription;
+        }
+
+        return response;
+    }
+}
---
+++ App_Code/FTPHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for FTPHelper
+/// </summary>
+public class FTPHelper
+{
+
+    private static string excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel {1};HDR={2};IMEX=1\";";
+
+    public DataSet ReadExcelFile(string filePath, Boolean hasHeader)
+    {
+        string version = Path.GetExtension(filePath) == ".xlsx" ? "12.0 Xml" : "8.0";
+        string sheetName = "Sheet1";
+        string cellRange = "";
+        var cmd = String.Format("SELECT * FROM [{0}${1}]", sheetName, cellRange);
+        excelConnectionString = String.Format(excelConnectionString, filePath, version, (hasHeader ? "YES" : "NO"));
+        var ds = new DataSet();
+        ds.Tables.Add(new DataTable(sheetName));
+
+        using (var connection = new OleDbConnection(excelConnectionString))
+        {
+            OleDbCommand command = new OleDbCommand(cmd, connection);
+            OleDbDataAdapter da = new OleDbDataAdapter(command);
+            da.FillSchema(ds.Tables[sheetName], SchemaType.Source);
+
+            foreach (DataColumn dc in ds.Tables[sheetName].Columns)
+            {
+                dc.DataType = typeof(String);
+                dc.ColumnName = dc.ColumnName.Trim();
+            }
+
+            da.Fill(ds.Tables[sheetName]);
+
+            //foreach (DataColumn dc in ds.Tables[sheetName].Columns) {
+            //    dc.ColumnName = dc.ColumnName.Trim();
+            //}
+        }
+
+        return ds;
+    }
+}
